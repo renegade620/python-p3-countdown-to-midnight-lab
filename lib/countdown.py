1cc291cbdcf2b0e1a661fc5cadf17034e74dbd96
Frankline Were <franklinegift@gmail.com>
2024-09-03 10:38:33 +0300

    while loop plus delay

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time # import time library to use sleep
+
+def countdown(num):
+    while(num >= 1):
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    while(num >= 1):
+        print(f'{num} SECOND(S)!')
+        time.sleep(1) # delays each step of the loop by 1 second
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+countdown_with_sleep(10)
